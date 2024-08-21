f00b45662da2128ca63d4ee5d8db4536b87f8340
sukkss <putuajus234@gmail.com>
2024-08-21 14:25:27 +0800

    Add files via upload

---
+++ ProjectHTML/button.js
@@ -0,0 +1,44 @@
+// const buttonDetail1 = document.getElementById('buttonDetail1');
+// const detail1Div = document.getElementById('detail1');
+
+// buttonDetail1.addEventListener('click', () => {
+//  if (detail1Div.style.display === 'none') {
+//     detail1Div.style.display = 'block';
+//  } else }
+//  detail1Div.style.display = 'none';
+// }
+
+// });
+
+const buttonDetail1 = document.getElementById('buttonDetail1');
+const detail1Div = document.getElementById('detail1');
+
+buttonDetail1.addEventListener('click', () => {
+    if (detail1Div.style.display === 'none') {
+        detail1Div.style.display = 'block';
+    } else {
+        detail1Div.style.display = 'none';
+    }
+});
+
+const buttonDetail2 = document.getElementById('buttonDetail2');
+const detail2Div = document.getElementById('detail2');
+
+buttonDetail2.addEventListener('click', () => {
+    if (detail2Div.style.display === 'none') {
+        detail2Div.style.display = 'block';
+    } else {
+        detail2Div.style.display = 'none';
+    }
+});
+
+const buttonDetail3 = document.getElementById('buttonDetail3');
+const detail3Div = document.getElementById('detail3');
+
+buttonDetail3.addEventListener('click', () => {
+    if (detail3Div.style.display === 'none') {
+        detail3Div.style.display = 'block';
+    } else {
+        detail3Div.style.display = 'none';
+    }
+});
